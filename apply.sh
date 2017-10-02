b939f1a2b45e9320f2906e4f4eb65ba0223799ba
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 12:09:37 +0200

    stop if any error is encountered

--- apply.sh
+++ apply.sh
@@ -4,6 +4,8 @@
 # directory with this file
 d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -e
+
 for workaround in $d/MDEV-* ; do
   [ -e $workaround/print_fix.sh ] && eval "$(bash $workaround/print_fix.sh)"
 done
