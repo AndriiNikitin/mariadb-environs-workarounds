bbf4e2373f8fdf5c9d0868bf4274b828d9a6f5b6
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 11:17:31 +0200

    .

--- MDEV-13969/is_fix_needed.sh
+++ MDEV-13969/is_fix_needed.sh
@@ -10,9 +10,9 @@ d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$version" != 10.1.28 ] && [ "$version" != 10.2.9 ] ; then
   echo no
-elif patch --dry-run --silent $(which wsrep_sst_mysqldump) d/wsrep_sst_mysqldump.patch &> /dev/null  ; then
+elif patch --dry-run --silent $(which wsrep_sst_mysqldump) "$d/wsrep_sst_mysqldump.patch" &> /dev/null  ; then
   echo yes
-elif patch -R --dry-run --silent $(which wsrep_sst_mysqldump) d/wsrep_sst_mysqldump.patch &> /dev/null  ; then
+elif patch -R --dry-run --silent $(which wsrep_sst_mysqldump) "$d/wsrep_sst_mysqldump.patch" &> /dev/null  ; then
   echo fixed
 else
   echo maybefixed
