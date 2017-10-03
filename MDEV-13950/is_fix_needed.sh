4a4f2724aff0f00fcb339bb44bfa15dbbdd2522d
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-03 09:22:44 +0200

    include currently developerd versions

--- MDEV-13950/is_fix_needed.sh
+++ MDEV-13950/is_fix_needed.sh
@@ -8,7 +8,9 @@ version=${version%%\-*}
 # directory of this script
 d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$version" != 10.1.28 ] && [ "$version" != 10.2.9 ]  ; then
+affected_versions='10.1.28 10.1.29 10.2.9 10.2.10'
+
+if [[ ! $affected_versions =~ "$version" ]]  ; then
   echo no
 elif patch    -f --dry-run --silent $(which mysqld_safe) "$d/mysqld_safe.patch" &>/dev/null ; then
   echo yes
--- MDEV-13969/is_fix_needed.sh
+++ MDEV-13969/is_fix_needed.sh
@@ -8,7 +8,9 @@ version=${version%%\-*}
 # directory of this script
 d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$version" != 10.1.28 ] && [ "$version" != 10.2.9 ] ; then
+affected_versions='10.1.28 10.1.29 10.2.9 10.2.10'
+
+if [[ ! $affected_versions =~ "$version" ]]  ; then
   echo no
 elif patch --dry-run --silent $(which wsrep_sst_mysqldump) "$d/wsrep_sst_mysqldump.patch" &> /dev/null  ; then
   echo yes
